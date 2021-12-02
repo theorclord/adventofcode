809415f761cc599c922fdf30d96fe1f05e0c56f2
theorclord <mstolbor@gmail.com>
2021-12-02 16:55:27 +0100

    initial commit. Day one

---
+++ AdventOfCode/Day1/DepthScan.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Advent of code 1
+    /// </summary>
+    class DepthScan
+    {
+        public DepthScan()
+        {
+
+        }
+
+        private static List<int> ReadFile(string filePath)
+        {
+            var readLines = File.ReadAllLines(filePath);
+
+            var inputScanParsed = readLines.Select(l => int.Parse(l)).ToList();
+            return readLines.Select(l => int.Parse(l)).ToList();
+        }
+
+        public static int CalculateNumberOfDepthIncreases(string filePath)
+        {
+            var inputScans = ReadFile(filePath);
+            var numberOfDepthIncreases = 0;
+            int? previousDepth = null;
+            foreach (var scanDepth in inputScans)
+            {
+                if (previousDepth != null && scanDepth > previousDepth)
+                {
+                    numberOfDepthIncreases++;
+                }
+
+                previousDepth = scanDepth;
+            }
+
+            return numberOfDepthIncreases;
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Day one
+            int numberOfDepthIncreases = DepthScan.CalculateNumberOfDepthIncreases(AppContext.BaseDirectory + @"\Day1\inputScanData.txt");
+
+            Console.WriteLine(numberOfDepthIncreases);
+
+
+            // Day two
+        }
+    }
+}
